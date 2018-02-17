71d9bf9126647b4f2559a2f8320b8333dcaa6520
sertana <basnetsanajaya7@gmail.com>
2018-02-17 16:56:19 -0500

    model for all activities

--- travel-server/models/noWander.js
+++ travel-server/models/noWander.js
@@ -2,5 +2,14 @@ const axios = require("axios");
 const db = require("../db/index.js");
 const noWander = {};
 
+noWander.allActivities = (req, res, next)=>{
+  db.manyOrNone('SELECT * FROM activity')
+  .then(activity => {
+    res.locals.activities = activities;
+    next();
+  })
+  .catch(err=>console.log(err));
+};
+
 
 module.exports = noWander;
