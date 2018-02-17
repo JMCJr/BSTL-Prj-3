1ba99e1aa7e39b04bfde5d0c819c841bd2dcc7aa
keithmccall <keithcodes@gmail.com>
2018-02-17 13:54:32 -0500

    schema added

---
+++ travel-server/db/schema.sql
@@ -0,0 +1,42 @@
+\c project_3
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  username VARCHAR(20) UNIQUE,
+  password_digest VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS itinerary;
+
+CREATE TABLE itinerary(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	city VARCHAR(255),
+	date_departing VARCHAR(255),
+	date_returning VARCHAR(255),
+	budget INTEGER,
+);
+
+DROP TABLE IF EXISTS users_itinerary;
+
+CREATE TABLE users_itinerary(
+ id SERIAL PRIMARY KEY,
+ user_id INTEGER,
+ itinerary_id INTEGER 
+);
+
+DROP TABLE IF EXISTS activity;
+
+CREATE TABLE activity(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	description VARCHAR(255),
+	price INTEGER,
+	votes INTEGER,
+	itinerary_id INTEGER
+);
