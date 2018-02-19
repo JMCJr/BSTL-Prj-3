189e355e402408400dca9d90f138e4198c49aa3e
sertana <basnetsanajaya7@gmail.com>
2018-02-19 14:31:00 -0500

    router defined in controllers and model for creating new activity

--- travel-server/controllers/noWander.js
+++ travel-server/controllers/noWander.js
@@ -3,12 +3,94 @@ const express = require("express"),
 	// the model:
 	noWander = require("../models/noWander.js");
 
+
+// ----------------------------------------------------
+// Define GET request for '/dashboard', which is ALL
+// noWanderModel.getAllItineraries
+// This is _______ job
 router.get("/dashboard", noWander.allItineraries, (req, res) => {
 	const { itineraries } = res.locals;
 	res.json({ itineraries });
 });
+
+// ----------------------------------------------------
+// Define POST request for '/newitinerary', which is ONE 
+// noWanderModel.NewItinerary
+// This is ______ job
+
 router.post("/dashboard", noWander.newItinerary, (req, res) => {
 	console.log(res.locals);
 	res.json(res.locals.itineraryId);
 });
+
+// ----------------------------------------------------
+// Define GET request for '/itinerary/:id', which is ALL 
+// noWanderModel.getAllActivities
+// This is Leon's job
+
+
+
+// NOTHING HERE FOR NOW
+
+
+
+// ----------------------------------------------------
+// ----------------------------------------------------
+// Define GET request for 'itinerary/:id', which is ONE 
+// noWanderModel.getOneItinerary
+// This is ______ job
+router.get('/itinerary/:id',noWander.getItinerary, (req, res, next) =>{
+  res.json(res.locals.getItineraryData);
+});
+
+
+
+
+
+
+
+
+// ----------------------------------------------------
+// ----------------------------------------------------
+// Define POST request for '/itinerary/:id/newactivity', which is ONE 
+// noWanderModel.NewActivity
+// This is ______ job
+
+
+
+
+
+
+
+
+// ----------------------------------------------------
+// Define DELETE request for '/dashboard', which is ONE
+// noWanderModel.deleteOneItinerary
+// This is _______ job
+router.delete('/dashboard', noWander.deleteItinerary, (req, res, next) =>{
+  res.json();
+});
+
+
+
+
+
+
+
+// ----------------------------------------------------
+// Define DELETE request for 'itinerary/:id', which is ONE 
+// noWanderModel.deleteOneActivity
+// This is ______ job
+router.delete('/itinerary/:id',noWander.deleteActivity,(req, res, next) =>{
+  res.json();
+});
+
+
+
+
+
+
+
+
+// ----------------------------------------------------
 module.exports = router;
