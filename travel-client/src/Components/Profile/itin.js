bf85bda9683bdf14689da2036a4c2c0bf0290d83
JMCJr <jorge.correa.jr@gmail.com>
2018-02-19 16:08:37 -0500

    correct capitalization problem file

--- travel-client/src/Components/Profile/itin.js
+++ travel-client/src/Components/Profile/itin.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
-
+import axios from "axios";
 
 
 export default class Itin extends Component {
