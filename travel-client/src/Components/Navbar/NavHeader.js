c4cf3be13a1ba6e72176c8b2c8426235ae3d0973
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-02-20 18:18:24 -0500

    dropdown works (basically)

--- travel-client/src/Components/Navbar/NavHeader.js
+++ travel-client/src/Components/Navbar/NavHeader.js
@@ -2,19 +2,39 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
 import DropdownButt from "./DropdownButt";
 
+
 export default class NavHeader extends Component {
 
-  
-    
+    constructor(props){
+      super(props);
+      this.handleOpenMenu = this.handleOpenMenu.bind(this)
+      this.state = {menuIsOpen: false}
+    }  
+  handleOpenMenu(e) {
+    e.preventDefault();
+    this.setState(prevState => {
+      const nextState = {menuIsOpen: !prevState.menuIsOpen };
+      return nextState;
+    });
+  }
+   
     render(){
+    let maybeMenuOpen = null;
+    if (this.state.menuIsOpen) {
+      maybeMenuOpen = (
+          <div className="dropWhole">
+          <DropdownButt />
+          </div>
+      );
+    }    
     return(
       <div className="navWhole">
       <Link to={'/dashboard'}><p className="profileIMG">Profile Image</p></Link>
       <Link to={'/landing'}><p className="signout">Sign out</p></Link>
-      <div></div>
-      <DropdownButt className="dropWhole" />
-
+      <button onClick={this.handleOpenMenu}/>
+      {maybeMenuOpen}
       </div>
-    )
+    )  
+
   }
 }
