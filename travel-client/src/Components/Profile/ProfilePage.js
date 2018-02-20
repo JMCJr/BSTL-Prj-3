f2d695366120c0f2f8284021cd92c13cacf28c4a
JMCJr <jorge.correa.jr@gmail.com>
2018-02-20 16:27:09 -0500

    JMC minor update to rectify merge conflict in ProfilePage

--- travel-client/src/Components/Profile/ProfilePage.js
+++ travel-client/src/Components/Profile/ProfilePage.js
@@ -1,26 +1,9 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
 import NavHeader from "../Navbar/NavHeader";
 import ItinList from "./ItinList";
 import NewItin from "./NewItin";
 import YourItinPage from "../YourItin/YourItinPage";
-
-=======
-import {
-	BrowserRouter as Router,
-	Link,
-	Route,
-	Switch,
-	Redirect
-} from "react-router-dom";
-
-import Nav from "../Nav";
-
-import NewItin from "./NewItin";
-import YourItinPage from "../YourItin/YourItinPage";
-import UserInfo from "./UserInfo";
->>>>>>> 63876702ca7613bb94aa599174b78baf37957085
 
 export default class ProfilePage extends Component {
 	constructor(props) {
@@ -39,15 +22,11 @@ export default class ProfilePage extends Component {
 						<div>CREATE NEW ITINERARY</div>
 					</Link>
 					<Switch>
-<<<<<<< HEAD
+
 						<Route exact path="/itinerary/:id" component={YourItinPage} />
 						<Route exact path="/newitinerary" component={NewItin} />
 						<Route exact path="/dashboard" component={ItinList} />
-=======
-						<Route exact path="/dashboard" component={UserInfo} />
-						<Route exact path="/newitinerary" component={NewItin} />
-						<Route path="/itinerary/:id" component={YourItinPage} />
->>>>>>> 63876702ca7613bb94aa599174b78baf37957085
+
 					</Switch>
 				</div>
 			</Router>
