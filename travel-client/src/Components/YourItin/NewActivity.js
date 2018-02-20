309222b167db9e58dec117c356972a663af7ae02
Leon Morgan <lendoe@aol.com>
2018-02-20 11:05:06 -0500

    added modal

---
+++ travel-client/src/Components/YourItin/NewActivity.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import axios from "axios";
+
+
+export default class NewActivity extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name: "",
+      description:"",
+      price:"",
+      votes: 0,
+      itinerary_id: props.itinerary.id
+    }
+
+  }
+changeActivity(ev) {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
+  }
+  createCall(newActivity) {
+    axios({
+      url: "http://localhost:8080/api/nowander/dashboard",
+      method: "POST",
+      data: newActivity
+    }).then(res => {
+      console.log(res);
+    });
+  }
+  submitActivity(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    this.createCall(this.state);
+  }
+
+
+  render() {
+    return (
+      <form onSubmit={this.submitActivity.bind(this)}>
+        <label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            placeholder="name"
+            onChange={this.changeActivity.bind(this)}
+          />
+        </label>
+        <label>
+          <br />
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+            placeholder="description"
+            onChange={this.changeActivity.bind(this)}
+          />
+        </label>
+        <label>
+          <br />
+          <input
+            type="text"
+            name="price"
+            value={this.state.price}
+            placeholder="price"
+            onChange={this.changeActivity.bind(this)}
+          />
+        </label>
+        <input type="submit"/>
+        <div className="Hidden">
+          <label>
+            <br />
+            <input
+              type="text"
+              name="votes"
+              value={this.state.votes}
+              placeholder="votes"
+              onChange={this.changeActivity.bind(this)}
+            />
+          </label>
+          <label>
+            <br />
+            <input
+              type="text"
+              name="itinerary_id"
+              value={this.state.itinerary_id}
+              placeholder="itinerary_id"
+              onChange={this.changeActivity.bind(this)}
+            />
+          </label>
+
+        </div>
+      </form>
+    );
+  }
+}
--- travel-client/src/Components/YourItin/YourItinInfo.js
+++ travel-client/src/Components/YourItin/YourItinInfo.js
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
-
+import Modal from 'react-responsive-modal';
+import NewActivity from './NewActivity';
 
 
 export default class YourItinInfo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modalOpen : false
+    }
+    this.modalHandler = this.modalHandler.bind(this);
+  }
+
+  modalHandler(){
+    this.setState(prevState=>{
+      prevState.modalOpen = !prevState.modalOpen;
+      return prevState;
+    });
+  }
+
+
+
   render() {
     const itinerary = this.props.itinerary;
+    const {modalOpen} = this.state
     return (
-      <div className="Itinerary-card">
-        <p>Name: {itinerary.name}</p>
-        <p>City: {itinerary.city}</p>
-        <p>Departing: {itinerary.date_departing}</p>
-        <p>Returning: {itinerary.date_returning}</p>
-        <p>Budget: ${itinerary.budget}</p>
-      </div>
+    <div>  
+        <div className="Itinerary-card">
+          <p>Name: {itinerary.name}</p>
+          <p>City: {itinerary.city}</p>
+          <p>Departing: {itinerary.date_departing}</p>
+          <p>Returning: {itinerary.date_returning}</p>
+          <p>Budget: ${itinerary.budget}</p>
+        </div>
+
+        <div>
+          <button onClick={this.modalHandler}>Open modal</button>
+          <Modal open={modalOpen} onClose={this.modalHandler} closeIconSize=
+          {8} little>
+            <NewActivity itinerary={itinerary}/>
+          </Modal>
+        </div>
+     </div> 
+
+
     );
   }
 }
