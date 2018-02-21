1e36dacf19984bacf55d6b3639f09a5918b3ff50
sertana <basnetsanajaya7@gmail.com>
2018-02-20 19:46:48 -0500

    deletesActivity

--- travel-client/src/Components/YourItin/Activity.js
+++ travel-client/src/Components/YourItin/Activity.js
@@ -6,15 +6,32 @@ import axios from "axios";
 export default class Activity extends Component {
   constructor(props){
     super(props);
+    this.deleteActivity=this.deleteActivity.bind(this);
+    this.deleteHandler=this.deleteHandler.bind(this);
   }
+ deleteActivity(id){
+  axios({
+    url:`http://localhost:8080/api/noWander/itinerary/${id}/activity`,
+  method:"DELETE"
+ }).then(response =>{
+  console.log(response);
+  // this.props.getActivities();
+ })
+ } 
+
+ deleteHandler(e){
+  e.preventDefault();
+  this.deleteActivity(this.props.itineraryID)
+ }
   render() {
     const activity = this.props.activity;
     return (
       <div className="Activity-card">
         <p>Name: {activity.name}</p>
         <p>Description: {activity.description}</p>
-        <p>Price: {activity.price}</p>        
+        <p>Price: {activity.price}</p>
+        <button onClick={this.deleteHandler}>Delete</button>        
       </div>
     );
   }
-};
+}
